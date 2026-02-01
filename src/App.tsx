c9e73fad643688fed324f84ee1134e46862ef5d3
Nidula Ekanayake <nidulanuwan@gmail.com>
2026-02-01 14:32:46 +0530

    Add project files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import ReactLogo from './assets/react.svg';
+
+function App() {
+  
+  return (
+  
+  <section>
+    <h1>Hello there</h1>
+    <img src={ReactLogo} alt="Vite logo" />
+  </section>
+
+ );
+ 
+}
+
+export default App
